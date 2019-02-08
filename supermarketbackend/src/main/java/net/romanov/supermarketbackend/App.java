2d6f3f78433ee2c173160451517a47ef39c7c9cc
21327851 <21327851@EBY03023D01109.STUDENT.TVU.AC.UK>
2019-02-08 10:27:30 +0000

    Rough guideline.

--- supermarketbackend/src/main/java/net/romanov/supermarketbackend/App.java
+++ supermarketbackend/src/main/java/net/romanov/supermarketbackend/App.java
@@ -4,13 +4,34 @@ package net.romanov.supermarketbackend;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("This is Sant");
-        System.out.println("This is Sam");
-        
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("This is Sant");
+		System.out.println("This is Sam");
+
+	}
+
+	public void getDB() {
+		// Area for getting DB info
+
+		updateSite();
+	}
+
+	public void updateSite() {
+		// Get info from getDB, push to website.
+
+	}
+
+	public void getLoginDetails() {
+		// Grabs Username and Password from Website textboxes
+
+		verifyUser(username, password);
+	}
+
+	public void verifyUser(String Username, String Password) {
+		// Checks username and password against the DB
+
+	}
+
 }
