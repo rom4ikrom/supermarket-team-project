4aebda4f8e22e8f3fc26b716567897365181574b
Roman Romanov <roman.romanov1998@gmail.com>
2019-03-07 19:21:10 +0000

    Modify git ignore file.

--- supermarketbackend/src/main/java/net/romanov/supermarketbackend/App.java
+++ supermarketbackend/src/main/java/net/romanov/supermarketbackend/App.java
@@ -12,26 +12,4 @@ public class App {
 
 	}
 
-	public void getDB() {
-		// Area for getting DB info
-
-		updateSite();
-	}
-
-	public void updateSite() {
-		// Get info from getDB, push to website.
-
-	}
-
-	public void getLoginDetails() {
-		// Grabs Username and Password from Website textboxes
-
-		verifyUser(username, password);
-	}
-
-	public void verifyUser(String Username, String Password) {
-		// Checks username and password against the DB
-
-	}
-
 }
